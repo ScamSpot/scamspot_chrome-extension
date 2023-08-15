ce6a35c25bf1aa2a7d3deda071f6fde0c6cd0ea6
Stefan <stefan.erben1@gmail.com>
2023-08-15 02:44:11 +0200

    Updated code as Instagram has changed its HTML DOM. Made the app more resilient to future changes

--- scripts/content.js
+++ scripts/content.js
@@ -1,56 +1,87 @@
-console.log("Instagram Spam Checker active!")
+console.log("ScamSpot is active!")
 
 const mode = 1; // 0 = label comments, 1 = hide comments
+
+const mobile_selector = ".x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.xt0psk2.x1i0vuye.x1fhwpqd.x1s688f.x1roi4f4.x10wh9bi.x1wdrske.x8viiok.x18hxmgj";
+const desktop_selector = ".x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft";
+
 
 // Select the body element
 const body = document.querySelector('body');
 
-// Observe the body element for changes to its children
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    //console.log(mutation);
-    if (String(mutation.target.innerHTML).includes('_a9ym')) {
-        //console.log(mutation);
+// Function to process comments
+function processMobileComment(comment) {
+  if (comment.textContent === "Reply") {
+    replyButton = comment.parentElement;
+    comment_container = replyButton.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    evaluateComment(comment_container, replyButton, "mobile");
+  }
+}
 
-        // Main Comment Load
-        //if (mutation.previousSibling.className === '_ae5m _ae5n _ae5o' && mutation.addedNodes.length > 0) { 
-        if (mutation.addedNodes.length > 0) { 
-          mutation.addedNodes[0].querySelectorAll('._a9ym').forEach((comment) => {
-            //console.log("Main Comment Load");
-            extractComment(comment);
-          });
-        }
-        
-        if (mutation.addedNodes[0] !== undefined && mutation.addedNodes[0].className === '_a9ym') {
-          extractComment(mutation.addedNodes[0]);
-        }
+// Function to process comments
+function processDesktopComment(comment) {
+  if (comment.textContent === "Reply") {    
+    replyButton = comment.parentElement.parentElement;
+    comment_container = replyButton.parentElement.parentElement.parentElement.parentElement.parentElement;
+    evaluateComment(comment_container, replyButton, "desktop");
+  }
+}
 
+// Function to process nodes
+function processNodes(nodes) {
+  nodes.forEach((node) => {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      
+      extractComment(node, mobile_selector);
+      extractComment(node, desktop_selector);
     }
   });
-});
+}
 
-observer.observe(body, {
-  childList: true,
-  subtree: true,
-});
+function extractComment(element, selector) {
+  if(selector === mobile_selector) {
+    element.querySelectorAll(mobile_selector)
+    .forEach((comment) => {
+      processMobileComment(comment);
+    });
+  } else if(selector === desktop_selector) {
+    element.querySelectorAll(desktop_selector).forEach((comment) => {
+      processDesktopComment(comment);
+    });
+  }
+}
+
+window.onload = function () {
+  // Process existing comments
+  extractComment(document, mobile_selector);
+  extractComment(document, desktop_selector);
+
+  // Observe the body element for changes to its children
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      processNodes(mutation.addedNodes);
+    });
+  });
+
+  // Configure and start observing
+  const observerConfig = { childList: true, subtree: true };
+  observer.observe(body, observerConfig);
+
+};
 
 
-async function extractComment(comment) {
-  // console.log(comment);
-  var text = comment.innerText || comment.textContent;
-  //console.log(text);
+async function evaluateComment(comment_container, replyButton, device) {
+  
+  var text_container = null;
+  if(device === "mobile") {
+    text_container = comment_container.querySelector('div._a9zs');
+  } else if(device === "desktop") {  
+    text_container = comment_container.querySelector('.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1cy8zhl.x1oa3qoh.x1nhvcw1');
+  }
+  if(text_container !== null) {
+    let text = text_container.textContent;
+    let hash = createHash(text);
 
-  if(text !== "Load more comments") {
-    //const result = text.split('\n');
-    const result = text.split('\n').filter(line => !line.includes('View replies'));
-
-    let username = result[0];
-    let commentText = result.slice(1, -1).join(' ');
-
-    let hash = createHash(result[0] + result[1]);
-
-    let data = { "username": username, "comment": commentText, "hash": hash };
-    
     // make request with data
     const request = new XMLHttpRequest();
     request.open('POST', 'https://spamspot-api-qkthc.ondigitalocean.app/scam/', true);
@@ -64,9 +95,7 @@ async function extractComment(comment) {
           var classifier = content.class;
           // var rating = content.score;
           category = categorizeRating(classifier);
-          
-          //console.log(identifier + ": " + rating);
-          
+                    
           // find report button
           const reportLink = document.querySelector('a#report-' + identifier);
           if (reportLink !== null) {
@@ -76,7 +105,17 @@ async function extractComment(comment) {
           
           // find span with id = rating
           const replace = document.querySelector('span#cr-' + identifier);
-          const comment_container = replace.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+          if(replace === null) {
+            return;
+          }
+          
+          var comment_container = null;
+          if(device === "mobile") {
+            comment_container = replace.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+          } else if(device === "desktop") {  
+            comment_container = replace.parentElement.parentElement.parentElement.parentElement.parentElement;
+          }
+        
           comment_container.style.filter = '';
           if (replace !== null) {
             replace.textContent = category.toUpperCase(); // + ' (' + category + ')';
@@ -102,17 +141,8 @@ async function extractComment(comment) {
     request.onerror = function () {
       console.error('Error: Network Error');
     };
-    request.send(JSON.stringify({comment_id: hash, comment_text: commentText}));
-
-    // console.log(comment)
-
-    // see image in docs & http://stevewellens.xtreemhost.com/jQuerySelectorTester.htm?i=1
-    // HTML = console.log(comment)
-    // Search Pattern = adapted JS Selector (remove the classes at the beginning)
+    request.send(JSON.stringify({comment_id: hash, comment_text: text}));
     
-    const buttons = comment.querySelectorAll('.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > span > button')
-    const replyButton = buttons[buttons.length - 1]
-    //console.log(replyButton);
 
     const ratingIdentifier = document.createElement('span');
     ratingIdentifier.id = "cr-" + hash;
@@ -120,20 +150,20 @@ async function extractComment(comment) {
     ratingIdentifier.textContent = 'Loading...'; // (' + hash + ')';
     
     if (mode === 1) {
-      const comment_container = replyButton.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
       comment_container.style.filter = 'blur(2px)';
     }
-
+  
     const reportError = document.createElement('a');
-    reportError.href = "https://hook.eu1.make.com/i2uffenbekw7dedrudi8rn364z95cp5v?comment=" + encodeURIComponent(commentText) + "&username=" + username;
+    reportError.href = "https://hook.eu1.make.com/i2uffenbekw7dedrudi8rn364z95cp5v?comment=" + encodeURIComponent(text); // + "&username=" + username;
     reportError.target = "_blank";
     reportError.id = "report-" + hash;
     reportError.className = 'report-Button';
     reportError.textContent = 'Report error';
     reportError.style.display = 'none';
-
+  
     replyButton.parentNode.insertBefore(reportError, replyButton.nextSibling);
     replyButton.parentNode.insertBefore(ratingIdentifier, replyButton.nextSibling);
+  
   }
 }
 
